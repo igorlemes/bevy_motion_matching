50cdbbaf6d1a7a62ee3d30260d4f10130d40abb5
Igor Lemes <igor.s.lemes@gmail.com>
2024-02-28 18:54:08 -0300

    Cleaned the code

--- src/systems/trigger_position.rs
+++ src/systems/trigger_position.rs
@@ -14,18 +14,12 @@ pub fn move_circle_y(
     );
     if let Some(trigger) = trigger {
         for mut transform in &mut query {
-            // Get the screen width and height
             if let Some(window) = window.iter().next() {
                 let height = window.height();
-                // Put the circle in the value percentage of the screen height
                 if trigger.button_type == GamepadButtonType::RightTrigger2 {
                     transform.translation.y = trigger.value * height * 0.45;
-                    info!("RightTrigger2 Pressed {}", trigger.value);
-                    info!("Moved: {}", transform.translation.x);
                 } else if trigger.button_type == GamepadButtonType::LeftTrigger2 {
                     transform.translation.y = - trigger.value * height * 0.45;
-                    info!("LeftTrigger2 Pressed {}", trigger.value);
-                    info!("Moved: {}", transform.translation.x);
                 }
             };
             
@@ -38,15 +32,11 @@ pub fn move_circle_x(
     window: Query<&Window, With<PrimaryWindow>>,
     mut query: Query<&mut Transform, With<ControllerTrigger>>,
 ) {
-    // Is there a window resize event?
     if let Some(_event) = window_resize_event.read().last() {
         for mut transform in &mut query {
-            // Get the screen width and height
             if let Some(window) = window.iter().next() {
                 let width = window.width();
-                // Put the circle in the value percentage of the screen height
                 transform.translation.x = width * 0.45;
-                info!("Moved: {}", transform.translation.x);
             };
 
         }
